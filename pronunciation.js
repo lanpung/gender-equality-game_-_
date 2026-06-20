974b2e8f1b2461628286bca9c6b4953db8b28aa4
lanpung <0025410299@student.dthu.edu.vn>
2026-06-20 13:14:52 +0700

    Add IPA pronunciations for common words
    Added a dictionary of IPA pronunciations for common words.

---
+++ pronunciation.js
@@ -0,0 +1,9 @@
+const ipa = {
+Equality:"/ɪˈkwɒləti/",
+Opportunity:"/ˌɒpəˈtjuːnəti/",
+Respect:"/rɪˈspekt/",
+Responsibility:"/rɪˌspɒnsəˈbɪləti/",
+Stereotype:"/ˈsteriətaɪp/",
+Participate:"/pɑːˈtɪsɪpeɪt/",
+Encourage:"/ɪnˈkʌrɪdʒ/"
+};
