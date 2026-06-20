7c66b09526a80e6844efd40c2bbaa2d72ba9ea7f
lanpung <0025410299@student.dthu.edu.vn>
2026-06-20 13:14:06 +0700

    Add questions, meanings, praises, and comforts arrays

---
+++ questions.js
@@ -0,0 +1,63 @@
+const words = [
+{
+word:"Equality",
+answer:"Sự bình đẳng"
+},
+{
+word:"Opportunity",
+answer:"Cơ hội"
+},
+{
+word:"Respect",
+answer:"Sự tôn trọng"
+},
+{
+word:"Responsibility",
+answer:"Trách nhiệm"
+},
+{
+word:"Stereotype",
+answer:"Định kiến"
+},
+{
+word:"Participate",
+answer:"Tham gia"
+},
+{
+word:"Encourage",
+answer:"Khuyến khích"
+}
+];
+
+const meanings = [
+"Sự bình đẳng",
+"Cơ hội",
+"Sự tôn trọng",
+"Trách nhiệm",
+"Định kiến",
+"Tham gia",
+"Khuyến khích",
+"Quyền lợi",
+"Tự do",
+"Giúp đỡ",
+"Hợp tác",
+"Sự công bằng"
+];
+
+const praises = [
+"🌟 Excellent!",
+"🌸 Amazing!",
+"🎉 Great Job!",
+"🩷 Fantastic!",
+"✨ Wonderful!",
+"🎀 Brilliant!",
+"🌈 Super!"
+];
+
+const comforts = [
+"🌼 Nice try!",
+"🩷 Don't give up!",
+"🌸 Keep learning!",
+"✨ You can do it!",
+"🌈 Try again next time!"
+];
